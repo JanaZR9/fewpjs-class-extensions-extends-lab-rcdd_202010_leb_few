c135e083a5fbff7aa69d0a244a6ce9330f66461c
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-14 12:57:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,20 @@
  }
    class Triangle extends Polygon{
     get isValid(){
-      if(this.sides[0]+this.sides[1]>this.sides[2] && this.sides[0]+this.sides[2]>this.sides[1] && this.sides[2]+this.sides[1]>this.sides[0] ) return true
+      if(this.sides[0]+this.sides[1]>this.sides[2] && this.sides[0]+this.sides[2]>this.sides[1] && this.sides[2]+this.sides[1]>this.sides[0] ) return true;
       else return false;
       
     }
+   }
+   
+   class Square extends Polygon {
+     get isValid(){
+     if(this.sides[0]==this.sides[1] && this.sides[2]==this.sides[3] && this.sides[0]==this.sides[2]) return true;
+     else return false;
+     }
+     
+     get area() {
+       return this.sides[0]*this.sides[1];
+     }
    }
  
